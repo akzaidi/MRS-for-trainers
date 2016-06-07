636697e765159218a80ce63bcef473197d8ccc9a
akzaidi <alikaz.zaidi@gmail.com>
2016-06-07 21:08:44 +0000

    rewrite summary function for sparkR

--- sparkR-ex.R
+++ sparkR-ex.R
@@ -1,5 +1,5 @@
 # kind of hold mirror for cran
-getOption('repos')
+r <- getOption('repos')
 # set mirror to something a bit more recent
 r["CRAN"] <- "https://mran.revolutionanalytics.com/snapshot/2016-06-07/"
 options(repos = r)
@@ -13,12 +13,13 @@ sample_taxi <- mutate(sample_taxi, tip_pct = sample_taxi$tip_amount/sample_taxi$
 taxi_hood_sum <- function(taxi_data = taxi_df, ...) {
   
   taxi_data %>% 
-    # filter(taxi_data$pickup_nhood %in% manhattan_hoods,
-    #        taxi_data$dropoff_nhood %in% manhattan_hoods, ...) %>% 
+    filter(taxi_data$pickup_nhood %in% manhattan_hoods) %>%
+    filter(taxi_data$dropoff_nhood %in% manhattan_hoods) %>% 
     group_by(taxi_data$dropoff_nhood, taxi_data$pickup_nhood) %>% 
-    summarize(ave_tip = mean("tip_pct"), 
-              ave_dist = mean("trip_distance")) %>% 
-    filter("ave_dist" > 3, "ave_tip" > 0.05) -> sum_df
+    summarize(ave_tip = mean(taxi_data$tip_pct), 
+              ave_dist = mean(taxi_data$trip_distance)) %>% 
+    filter("ave_tip > 0.05") %>% 
+    filter("ave_dist > 3") -> sum_df
   
   return(sum_df)
   
